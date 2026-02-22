2b2ef707a678343d500ceed74308bb367a4678b9
Ananza008z <anansit1111@gmail.com>
2026-02-23 02:30:35 +0700

    Create LoginController.cs

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using event_web_dev_project.Models;
+
+namespace event_web_dev_project.Controllers;
+
+public class LoginController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
